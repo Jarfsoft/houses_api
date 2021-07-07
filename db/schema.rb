9ef65dbfe655522122d1dd6ad5cd55d1ac995e48
Jarfsoft <andresjex@hotmail.com>
2021-07-07 02:18:16 -0600

    Change database DEVELOPMENT to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_06_30_093119) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "devices", force: :cascade do |t|
     t.string "name"
     t.text "description"
